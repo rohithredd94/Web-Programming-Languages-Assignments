70c080afaa1ecf77aef440abace28f771c71912a
Rohith Reddy <rohithreddyk94@gmail.com>
2017-06-24 15:27:02 -0500

    Email validation done

--- js/validate.js
+++ js/validate.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
 		var username = $("#username");
 		if(username.val().length != 0){
 			console.log("Found Content");
-			var pattern = /^[0-9a-zA-Z]+$/;
+			var pattern = /^[a-zA-Z0-9]+$/;
 			if(username.val().match(pattern)){
 				console.log("valid pattern");
 				var element = document.getElementById("username").nextSibling;
@@ -87,6 +87,21 @@ $(document).ready(function() {
 		var email = $("#email");
 		if(email.val().length != 0){
 			console.log("Found Content");
+			//var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z-:]{2,50}$/;
+			if(pattern.test(email.val())){
+				console.log("valid");
+				var element = document.getElementById("email").nextSibling;
+				var node = document.createTextNode("OK");
+				element.appendChild(node);
+				$(element).addClass("ok");
+			}else{
+				console.log("invalid");
+				var element = document.getElementById("email").nextSibling;
+				var node = document.createTextNode("Error");
+				element.appendChild(node);
+				$(element).addClass("error");
+			}
 		}
 	});
 });
