960e1b0915f2f4c958485d2b53b8ab5e372e496b
Rohith Reddy <rohithreddyk94@gmail.com>
2017-06-24 14:28:38 -0500

    Reduced overhead of creating too many span element

--- js/validate.js
+++ js/validate.js
@@ -1,51 +1,68 @@
 $(document).ready(function() {
 	// your js code goes here...
+	var para = document.createElement("span");
+	para.setAttribute("id", "span1");
+	$(para).insertAfter("#username");
+	
+	para = document.createElement("span");
+	para.setAttribute("id", "span2");
+	$(para).insertAfter("#password");
+
+	para = document.createElement("span");
+	para.setAttribute("id", "span3");
+	$(para).insertAfter("#email");
 
 	$("#username").focusin(function(){
-		console.log("Inside");
-		$("#span1").remove();
-		var para = document.createElement("span");
+		//console.log("Inside");
+		$("#span1").empty();
+		var para = document.getElementById("username").nextSibling;
 		var node = document.createTextNode("infoMessage");
 		para.appendChild(node);
-		para.setAttribute("id", "span1");
 		$(para).addClass("info");
-		var child = document.getElementById("username");
-		console.log(para);
-		$(para).insertAfter("#username");
+		//$("#span1").html("node");
+		//para.setAttribute("id", "span1");
+		//var child = document.getElementById("username");
+		//console.log(para);
+		//$(para).insertAfter("#username");
 	});
 	$("#username").focusout(function(){
-		$("#span1").remove();
+		$("#span1").empty().removeClass("info");
+		var username = $("#username");
+		if(username.val().length != 0){
+			console.log("Found Content");
+
+		}
 	});
 
 	$("#password").focusin(function(){
-		console.log("Inside");
-		$("#span2").remove();
-		var para = document.createElement("span");
+		//console.log("Inside");
+		$("#span2").empty();
+		var para = document.getElementById("password").nextSibling;
 		var node = document.createTextNode("infoMessage");
 		para.appendChild(node);
-		para.setAttribute("id", "span2");
 		$(para).addClass("info");
-		var child = document.getElementById("password");
-		console.log(para);
-		$(para).insertAfter("#password");
 	});
 	$("#password").focusout(function(){
-		$("#span2").remove();
+		$("#span2").empty().removeClass("info");
+		var password = $("#password");
+		if(password.val().length != 0){
+			console.log("Found Content");
+		}
 	});
 
 	$("#email").focusin(function(){
-		console.log("Inside");
-		$("#span3").remove();
-		var para = document.createElement("span");
+		//console.log("Inside");
+		$("#span3").empty();
+		var para = document.getElementById("email").nextSibling;
 		var node = document.createTextNode("infoMessage");
 		para.appendChild(node);
-		para.setAttribute("id", "span3");
 		$(para).addClass("info");
-		var child = document.getElementById("email");
-		console.log(para);
-		$(para).insertAfter("#email");
 	});
 	$("#email").focusout(function(){
-		$("#span3").remove();
+		$("#span3").empty().removeClass("info");
+		var email = $("#email");
+		if(email.val().length != 0){
+			console.log("Found Content");
+		}
 	});
 });
