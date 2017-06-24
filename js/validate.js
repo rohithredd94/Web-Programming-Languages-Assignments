58a915f35134d255aa22d943ed3193a1b6142dc8
Rohith Reddy <rohithreddyk94@gmail.com>
2017-06-24 16:58:45 -0500

    Optimized javascript code

--- js/validate.js
+++ js/validate.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	$("#username").focusin(function(){
 		//console.log("Inside");
 		$("#span1").empty().removeClass("ok").removeClass("error");
-		var element = document.getElementById("username").nextSibling;
+		var element = document.getElementById("span1")//.nextSibling;
 		var node = document.createTextNode("infoMessage");
 		element.appendChild(node);
 		$(element).addClass("info");
@@ -29,17 +29,17 @@ $(document).ready(function() {
 		$("#span1").empty().removeClass("info").removeClass("ok").removeClass("error");
 		var username = $("#username");
 		if(username.val().length != 0){
-			console.log("Found Content");
+			//console.log("Found Content");
 			var pattern = /^[a-zA-Z0-9]+$/;
 			if(username.val().match(pattern)){
-				console.log("valid pattern");
-				var element = document.getElementById("username").nextSibling;
+				//console.log("valid pattern");
+				var element = document.getElementById("span1");
 				var node = document.createTextNode("OK");
 				element.appendChild(node);
 				$(element).addClass("ok");
 			}else{
-				console.log("invalid pattern");
-				var element = document.getElementById("username").nextSibling;
+				//console.log("invalid pattern");
+				var element = document.getElementById("span1");
 				var node = document.createTextNode("Error");
 				element.appendChild(node);
 				$(element).addClass("error");
@@ -50,7 +50,7 @@ $(document).ready(function() {
 	$("#password").focusin(function(){
 		//console.log("Inside");
 		$("#span2").empty().removeClass("ok").removeClass("error");
-		var element = document.getElementById("password").nextSibling;
+		var element = document.getElementById("span2");
 		var node = document.createTextNode("infoMessage");
 		element.appendChild(node);
 		$(element).addClass("info");
@@ -59,14 +59,14 @@ $(document).ready(function() {
 		$("#span2").empty().removeClass("info").removeClass("ok").removeClass("error");
 		var password = $("#password");
 		if(password.val().length != 0){
-			console.log("Found Content");
+			//console.log("Found Content");
 			if(password.val().length >= 8){
-				var element = document.getElementById("password").nextSibling;
+				var element = document.getElementById("span2");
 				var node = document.createTextNode("OK");
 				element.appendChild(node);
 				$(element).addClass("ok");
 			}else{
-				var element = document.getElementById("password").nextSibling;
+				var element = document.getElementById("span2");
 				var node = document.createTextNode("Error");
 				element.appendChild(node);
 				$(element).addClass("error");
@@ -77,7 +77,7 @@ $(document).ready(function() {
 	$("#email").focusin(function(){
 		//console.log("Inside");
 		$("#span3").empty().removeClass("ok").removeClass("error");
-		var element = document.getElementById("email").nextSibling;
+		var element = document.getElementById("span3");
 		var node = document.createTextNode("infoMessage");
 		element.appendChild(node);
 		$(element).addClass("info");
@@ -86,18 +86,18 @@ $(document).ready(function() {
 		$("#span3").empty().removeClass("info").removeClass("ok").removeClass("error");
 		var email = $("#email");
 		if(email.val().length != 0){
-			console.log("Found Content");
+			//console.log("Found Content");
 			//var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 			var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z-:]{2,50}$/;
 			if(pattern.test(email.val())){
-				console.log("valid");
-				var element = document.getElementById("email").nextSibling;
+				//console.log("valid");
+				var element = document.getElementById("span3");
 				var node = document.createTextNode("OK");
 				element.appendChild(node);
 				$(element).addClass("ok");
 			}else{
-				console.log("invalid");
-				var element = document.getElementById("email").nextSibling;
+				//console.log("invalid");
+				var element = document.getElementById("span3");
 				var node = document.createTextNode("Error");
 				element.appendChild(node);
 				$(element).addClass("error");
